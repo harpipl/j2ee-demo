4f75d6c709e3f271ca5ee73235813904afe79759
adar-soft <arkadiusz.adamczak@gmail.com>
2017-03-21 18:27:02 +0100

    Create BaseResource

--- j2ee-demo-model/src/main/java/pl/harpi/samples/j2ee/demo/model/repository/jpa/JPAPersonRepository.java
+++ j2ee-demo-model/src/main/java/pl/harpi/samples/j2ee/demo/model/repository/jpa/JPAPersonRepository.java
@@ -6,19 +6,40 @@ import pl.harpi.samples.j2ee.demo.model.base.JPABaseRepository;
 import pl.harpi.samples.j2ee.demo.model.entity.Person;
 
 import javax.ejb.Stateless;
+import javax.persistence.Query;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Stateless
 public class JPAPersonRepository extends JPABaseRepository<Person> implements PersonRepository {
     @Override
     public List<Person> search(PersonSearchVO findVO) {
-        // TODO
-        return null;
+        String sql = "SELECT p FROM Person p WHERE 1=1 ";
+
+        if (findVO.getFirstName() != null) {
+            sql += " AND firstName = :firstName ";
+        }
+
+        if (findVO.getLastName() != null) {
+            sql += " AND lastName = :lastName ";
+        }
+
+        Query query = this.entityManager.createQuery(sql);
+
+        if (findVO.getFirstName() != null) {
+            query.setParameter("firstName", findVO.getFirstName());
+        }
+
+        if (findVO.getLastName() != null) {
+            query.setParameter("lastName", findVO.getLastName());
+        }
+
+        return query.getResultList();
     }
 
     @Override
     public List<Long> searchIds(PersonSearchVO findVO) {
-        // TODO
-        return null;
+        List<Person> docList = search(findVO);
+        return docList.stream().map(sc -> sc.getId()).collect(Collectors.toList());
     }
 }
--- j2ee-demo-service/src/main/java/pl/harpi/samples/j2ee/demo/service/rest/repository/PersonResource.java
+++ j2ee-demo-service/src/main/java/pl/harpi/samples/j2ee/demo/service/rest/repository/PersonResource.java
@@ -1,11 +1,13 @@
 package pl.harpi.samples.j2ee.demo.service.rest.repository;
 
+import pl.harpi.samples.j2ee.base.service.rest.BaseResource;
 import pl.harpi.samples.j2ee.demo.api.exceptions.ApplicationException;
 import pl.harpi.samples.j2ee.demo.api.model.PersonDTO;
 import pl.harpi.samples.j2ee.demo.api.model.PersonDTOBuilder;
 import pl.harpi.samples.j2ee.demo.api.model.PersonLocal;
 import pl.harpi.samples.j2ee.demo.service.rest.RestResponseFactory;
 
+import javax.ejb.EJBException;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
@@ -19,36 +21,42 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 @Path("/v1/repository/persons")
-public class PersonResource {
+public class PersonResource extends BaseResource {
     @Inject
     private PersonLocal personService;
 
     @GET
     @Path("/{personId}")
-    public Response getPerson(@PathParam("personId") String personId, @Context HttpServletRequest request) {
+    public Response getPerson(@PathParam("personId") String personId) {
         try {
             PersonDTO person = personService.getPersonById(Long.valueOf(personId));
-            return Response.status(Response.Status.OK).entity(RestResponseFactory.createPersonResponse(person, request)).build();
+            return Response.status(Response.Status.OK).entity(RestResponseFactory.createPersonResponse(person, this.getHttpRequest())).build();
         } catch (ApplicationException e) {
             return Response.status(Response.Status.CONFLICT).build();
         }
     }
 
     @GET
-    public Response getPersons(@Context HttpServletRequest request) {
+    public Response getPersons() {
         List<PersonDTO> persons = personService.getAllPersons();
 
-        return Response.status(Response.Status.OK).entity(RestResponseFactory.createPersonResponseList(persons, request)).build();
+        return Response.status(Response.Status.OK).entity(RestResponseFactory.createPersonResponseList(persons, this.getHttpRequest())).build();
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response createPerson(PersonRequest personRequest, @Context HttpServletRequest request) {
+    public Response createPerson(PersonRequest personRequest) {
         PersonDTO person = new PersonDTOBuilder()
                 .withFirstName(personRequest.getFirstName())
                 .withLastName(personRequest.getLastName())
                 .build();
 
-        return Response.status(Response.Status.CREATED).entity(RestResponseFactory.createPersonResponse(person, request)).build();
+        try {
+            person = personService.savePerson(person);
+            return Response.status(Response.Status.CREATED).entity(RestResponseFactory.createPersonResponse(person, this.getHttpRequest())).build();
+        } catch (EJBException | ApplicationException e) {
+            return Response.status(Response.Status.CONFLICT).build();
+        }
+
     }
 }
