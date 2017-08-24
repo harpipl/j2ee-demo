08b00de78d118d1c53af4e9d11bf5ad06b3f36e6
harpipl <arkadiusz.adamczak@gmail.com>
2017-08-24 21:34:55 +0200

    Changed type from JPAPersonQuery to Query in class PersonRepository

--- j2ee-demo-app/src/main/java/pl/harpi/samples/j2ee/demo/model/repository/PersonRepository.java
+++ j2ee-demo-app/src/main/java/pl/harpi/samples/j2ee/demo/model/repository/PersonRepository.java
@@ -1,10 +1,11 @@
 package pl.harpi.samples.j2ee.demo.model.repository;
 
+import pl.harpi.samples.j2ee.demo.api.base.model.Query;
 import pl.harpi.samples.j2ee.demo.api.model.PersonSearchVO;
 import pl.harpi.samples.j2ee.demo.model.base.BaseRepository;
 import pl.harpi.samples.j2ee.demo.model.entity.Person;
 import pl.harpi.samples.j2ee.demo.model.repository.jpa.JPAPersonQuery;
 
 public interface PersonRepository extends BaseRepository<Person, PersonSearchVO> {
-    JPAPersonQuery createPersonQuery();
+    Query createPersonQuery();
 }
