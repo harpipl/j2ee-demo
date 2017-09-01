1a071b0e61f604dc618383bb91f60f6333571dc6
harpipl <arkadiusz.adamczak@gmail.com>
2017-09-01 19:13:10 +0200

    SONAR: added private constructor

--- j2ee-demo-app/src/main/java/pl/harpi/samples/j2ee/demo/service/rest/RestUrls.java
+++ j2ee-demo-app/src/main/java/pl/harpi/samples/j2ee/demo/service/rest/RestUrls.java
@@ -1,6 +1,8 @@
 package pl.harpi.samples.j2ee.demo.service.rest;
 
 public final class RestUrls {
+    private RestUrls() {}
+
     private static final String SEGMENT_VERSION_1 = "v1";
 
     private static final String SEGMENT_REPOSITORY_RESOURCES = "repository";
@@ -9,5 +11,5 @@ public final class RestUrls {
     /**
      * URL template for a single person: <i>repository/persons/{0:personId}</i>
      */
-    public static final String[] URL_PERSON = {SEGMENT_VERSION_1, SEGMENT_REPOSITORY_RESOURCES, SEGMENT_PERSON_RESOURCE, "{0}"};
+    static final String[] URL_PERSON = {SEGMENT_VERSION_1, SEGMENT_REPOSITORY_RESOURCES, SEGMENT_PERSON_RESOURCE, "{0}"};
 }
